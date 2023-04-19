23e937071e6930eba9348602e345eee82191da0e
Alfayed <baksha97@gmail.com>
2023-04-18 23:08:10 -0400

    cleanup

--- Blipt/Views/Screens/CreateReceiptScreenView.swift
+++ Blipt/Views/Screens/CreateReceiptScreenView.swift
@@ -9,20 +9,26 @@ struct CreateReceiptScreenView: View {
   var body: some View {
     NavigationView {
       VStack {
-        ReceiptView(title: "Create Receipt", items: items) { item in
-          items.removeAll(where: { $0.id == item.id })
-        }
+        ReceiptView(title: "Create Receipt",
+                    items: items,
+                    onItemDeleteSwipe: onReceiptRemove)
         AddItemForm(onAddTap: onAddFormSubmit)
-        
-        NavigationLink {
-          SplitBillScreenView(viewModel: .init(people: [], receipt: items))
-        } label: {
-          Text("Done")
-        }
-        
-        
+        nextScreenButton
       }
     }
+  }
+  
+  @ViewBuilder
+  var nextScreenButton: some View {
+    NavigationLink {
+      SplitBillScreenView(viewModel: .init(people: [], receipt: items))
+    } label: {
+      Text("Done")
+    }
+  }
+  
+  func onReceiptRemove(item: ReceiptItem) {
+    items.removeAll(where: { $0.id == item.id })
   }
   
   func onAddFormSubmit(name: String?, cost: Double, quantity: Int) {
@@ -42,98 +48,3 @@ struct CreateReceiptScreenView_Previews: PreviewProvider {
     CreateReceiptScreenView()
   }
 }
-
-
-//import SwiftUI
-//import NumberTextField
-//
-//struct CreateReceiptScreenView: View {
-//  @State private var receiptItems = [ReceiptItem]()
-//  @State private var itemName = ""
-//  @State private var itemCost: Double = 0.0
-//  @State private var itemQuantity = 1
-//
-//  var itemQuantityValue: Int {
-//    receiptItems.filter({ $0.name.contains("Item #\(itemCount)") }).count + 1
-//  }
-//
-//  var itemCount: Int {
-//    receiptItems.count + 1
-//  }
-//
-//  var body: some View {
-//    NavigationView {
-//      VStack {
-//        List(receiptItems) { item in
-//          HStack {
-//            Text(item.name)
-//            Spacer()
-//            Text("\(item.cost)")
-//          }
-//        }
-//        Form {
-//          Section(header: Text("Add Item")) {
-//            TextField("Item Name (Optional)", text: $itemName)
-//              .font(.title3)
-//              .foregroundColor(.primary)
-//              .padding(.vertical, 8)
-//            TextField("Cost", value: $itemCost, formatter: NumberFormatter.currency)
-//              .keyboardType(.decimalPad)
-//              .font(.title3)
-//              .foregroundColor(.primary)
-//              .padding(.vertical, 8)
-//            Picker("Quantity", selection: $itemQuantity) {
-//              ForEach((1...15).reversed(), id: \.self) {
-//                Text("\($0)")
-//              }
-//            }
-//            .padding(.vertical, 8)
-//          }
-//          Button(action: {
-//            let itemName = self.itemName.isEmpty ? "Item #\(itemCount) - (\(itemQuantityValue) of \(itemQuantity))" : self.itemName
-//            let cost = itemCost
-//            guard cost > 0, itemQuantity > 0 else {
-//              return
-//            }
-//            (1...itemQuantity).forEach { index in
-//              let name = "\(itemName.replacingOccurrences(of: "\(itemQuantityValue) of \(itemQuantity)", with: "\(index) of \(itemQuantity)"))"
-//              receiptItems.append(
-//                ReceiptItem(
-//                  name: name,
-//                  cost: cost
-//                )
-//              )
-//            }
-//
-//            self.itemName = ""
-//            self.itemCost = 0.0
-//            self.itemQuantity = 1
-//          }) {
-//            Text("Add Item")
-//              .font(.headline)
-//              .foregroundColor(.white)
-//              .padding(.vertical, 10)
-//              .frame(maxWidth: .infinity)
-//              .background(Color.blue)
-//              .cornerRadius(10)
-//          }
-//        }
-//        .padding(.horizontal)
-//        .navigationBarTitle("Add Receipt Item", displayMode: .inline)
-//      }
-//    }
-//  }
-//
-//  func onAddTap() {
-//
-//  }
-//}
-//
-//
-//
-//
-//struct CreateReceiptScreenView_Previews: PreviewProvider {
-//  static var previews: some View {
-//    CreateReceiptScreenView()
-//  }
-//}
